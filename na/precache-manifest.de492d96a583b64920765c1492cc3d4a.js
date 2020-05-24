4ed0fb88348ec605ff20b957579aefe1790aba10
rajansrohit <rohitrajan@Rohits-MBP.attlocal.net>
2020-05-23 23:03:20 -0500

    Updates

---
+++ na/precache-manifest.de492d96a583b64920765c1492cc3d4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39c47e660008f15ded52d06617ce5906",
+    "url": "/react-coronasim/na/index.html"
+  },
+  {
+    "revision": "eae3fac1fd85e8f0f30a",
+    "url": "/react-coronasim/na/static/css/main.b0f610b5.chunk.css"
+  },
+  {
+    "revision": "4cfdaa5e69474338742e",
+    "url": "/react-coronasim/na/static/js/2.aea7d98c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-coronasim/na/static/js/2.aea7d98c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eae3fac1fd85e8f0f30a",
+    "url": "/react-coronasim/na/static/js/main.e850fda4.chunk.js"
+  },
+  {
+    "revision": "6832a70b0629e22e9ca4",
+    "url": "/react-coronasim/na/static/js/runtime-main.3e8e35e9.js"
+  }
+]);
