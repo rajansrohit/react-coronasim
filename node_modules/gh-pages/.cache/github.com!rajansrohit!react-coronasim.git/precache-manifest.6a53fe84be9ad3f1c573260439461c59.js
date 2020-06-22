5125c215c7ced863604e40c322e4391ad3fc6bfe
rajansrohit <rohitrajan@Rohits-MBP.attlocal.net>
2020-06-22 09:25:28 -0500

    commit

---
+++ node_modules/gh-pages/.cache/github.com!rajansrohit!react-coronasim.git/precache-manifest.6a53fe84be9ad3f1c573260439461c59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04864023a751e954f91f6ed67acde74b",
+    "url": "/react-coronasim/index.html"
+  },
+  {
+    "revision": "0231b157c5b541c681ac",
+    "url": "/react-coronasim/static/css/main.b0f610b5.chunk.css"
+  },
+  {
+    "revision": "4cfdaa5e69474338742e",
+    "url": "/react-coronasim/static/js/2.aea7d98c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-coronasim/static/js/2.aea7d98c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0231b157c5b541c681ac",
+    "url": "/react-coronasim/static/js/main.099eaec3.chunk.js"
+  },
+  {
+    "revision": "e3ff79dc64f174f40d71",
+    "url": "/react-coronasim/static/js/runtime-main.9e23a766.js"
+  }
+]);
